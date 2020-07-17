5a4d7b66ead2b8581e035ab8e23c3a98ee4ad29c
Aishwarya Surwase <aishwaryasurvase1998@gmail.com>
2020-07-18 01:39:19 +0530

    Changes in Error banner

---
+++ src/app/components/error-banner/error-banner.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from "@angular/core";
+
+@Component({
+  selector: "app-error-banner",
+  templateUrl: "./error-banner.component.html",
+  styleUrls: ["./error-banner.component.css"],
+})
+export class ErrorBannerComponent implements OnInit {
+  @Input("displayErrorText") displayErrorText: string;
+  @Input("links") links: any;
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log("Display text", this.displayErrorText);
+  }
+}
